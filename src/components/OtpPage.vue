cdf043e6e77ce4d7d4a74a93f3c73465f4c91afe
Aldo Aldiero <alexander@privyid.tech>
2023-02-14 16:57:37 +0700

    fix: otp title

--- src/components/OtpPage.vue
+++ src/components/OtpPage.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="flex flex-col justify-center items-center min-h-[80vh]">
+    <h1 class="mb-4 font-extrabold text-2xl">OTP</h1>
     <div
       class="rounded-lg shadow-xl p-4 max-w-[20rem] border-2 border-gray-300 border-opacity-25"
       elevation="0"
